00907f409aa9351a57d55f68d717cc8c82f08b7c
Camden Bickel <26368233+camden@users.noreply.github.com>
2021-06-12 14:58:13 -0700

    integrate w notion (stub)

---
+++ src/getNotionPage.js
@@ -0,0 +1,11 @@
+import { Client } from "@notionhq/client";
+
+const notion = new Client({ auth: process.env.NOTION_KEY });
+
+const dbId = process.env.NOTION_DATABASE_ID;
+
+const getNotionPage = (name) => {
+  //
+};
+
+export default getNotionPage;
